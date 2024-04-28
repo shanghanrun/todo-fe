cb29ba4a6c19c94ed6e8418161a7909a467bae7c
shanghanrun <idim7@naver.com>
2024-04-28 16:06:57 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
+import {useState} from 'react'
 
+function PrivateRoute({user, children}){
+  return (
+    user? children : <Navigate to='/login' />
+  )
+}
 
 function App() {
+  const [user, setUser] = useState('')
+
+  const getUser = async()=>{
+    try{
+      const token = sessionStorage.getItem('token')
+      //토큰이 유효한 지 백엔드에 검증요청
+
+    }catch(e){
+
+    }
+  }
     return (
       <Routes>
-        <Route path="/" element={<TodoPage />} />
+        <Route path="/" element={
+            <PrivateRoute>
+              <TodoPage />
+            </PrivateRoute>} />
         <Route path="/register" element={<RegisterPage />} />
 
         <Route path="/login" element={<LoginPage />} />
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,12 +9,13 @@ const LoginPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+  const [user, setUser] = useState(null) // 유저정보 저장
   const navigate = useNavigate()
 
   const handleLogin =async(e)=>{
     e.preventDefault();
-    // userInfo에 있는 email, password(암호화) 와 입력된 email, password가 일치할 경우 TodoPage로 이동하게 한다. 아니면 error메시지 (에러 핸들링)
-    try{  // 백엔드에 로그인에 관한 컨트롤러(loginWithEmail) 준비되어 있다. 그래서 성공하면 유저정보와 토큰을 프론트앤드로 보내준다.
+    try{  
+      // 백엔드에 로그인에 관한 컨트롤러(loginWithEmail) 준비되어 있다. 그래서 성공하면 유저정보와 토큰을 프론트앤드로 보내준다.
       // 로그인주소는  localhost:5000/api/user/login 이다.
       // register주소는 localhost:5000/api/user이다.
       console.log('email password :', email, password)
@@ -22,14 +23,18 @@ const LoginPage = () => {
       
       console.log('로그인 성공시 resp:', resp)
       if(resp.status ===200){
-
-        // navigate('/')  // 일단 TodoPage로 이동
+        setUser(resp.data.user)
+        sessionStorage.setItem("token", resp.data.token)
+        api.defaults.headers['authorization'] = "Bearer "+resp.data.token
+        setError('')
+        navigate('/')  // 일단 TodoPage로 이동
       } else{
         throw new Error(resp.message)
       }
     }catch(e){
       setError(e.message)
-      setEmail(''); setPassword('')
+      document.getElementById('formBasicEmail').value = ''; 
+      document.getElementById('formBasicPassword').value = '';
     }
   }
   return (
@@ -42,14 +47,14 @@ const LoginPage = () => {
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" 
-            onClick={(e)=>setEmail(e.target.value)}
+            onChange={(e)=>setEmail(e.target.value)}
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" 
-            onClick={(e)=>setPassword(e.target.value)}
+            onChange={(e)=>setPassword(e.target.value)}
           />
         </Form.Group>
         <div className="button-box">
